6e1f43c74d325de25fa21a2c919d1415c83181a0
Wilde Valdez <wildensx@users.noreply.github.com>
2023-10-20 01:46:14 -0400

    adding instance of webdrivers from most browsers

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,45 @@
 package org.example;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.sql.Driver;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args)
     {
-        //Invoke
-        ChromeDriver driver = new ChromeDriver();
+        //Invoke Chrome
+
+        //System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+        //WebDriver driver = new ChromeDriver();
+        //driver.get("https://the-internet.herokuapp.com/");
+        //driver.getTitle();
+        //System.out.println(driver.getTitle());
+        //driver.getCurrentUrl();
+        //System.out.println(driver.getCurrentUrl());
+        //driver.quit();
+
+        //Invoke Firefox
+
+        //System.setProperty("webdriver.gecko.driver", "drivers/geckodriver.exe");
+        //WebDriver driver = new FirefoxDriver();
+        //driver.get("https://the-internet.herokuapp.com/");
+        //driver.quit();
+
+        //Invoke Edge
+
+        System.setProperty("webdriver.edge.driver", "drivers/msedgedriver.exe");
+        WebDriver driver = new EdgeDriver();
+        driver.get("https://the-internet.herokuapp.com/");
+        driver.getTitle();
+        System.out.println(driver.getTitle());
+        driver.getCurrentUrl();
+        System.out.println(driver.getCurrentUrl());
+        driver.quit();
+
 
     }
 }
